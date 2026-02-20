f0c1f68f195417bd0624aad44e604b3cc415f650
ashutosh <ashutosh.g@magureinc.com>
2026-02-20 22:37:51 +0530

    Added the Notes and code

---
+++ code/day_04_input_output.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+	
+//	cout << "Hello Megha singh" << endl <<  "and krish Agrawal" ;
+//	cout << "Hello Ashutosh " << " sarthak" ;
+//	cout << "Hello Ashutosh  Sarthak" ;
+
+//	cout << "Ashutosh" << endl << "Giri" ;
+
+//	cout << "Ashutosh\nGiri" ;
+//	cout << "Suyash says" 
+//	<< "\n == endl "  ;
+
+//	cout <<  "student \n says is all endl clear" << endl <<
+//	"It makes me \n happy endl " ;
+
+//	int a , b ;
+//	cin >> a >> b ;
+//	cout << "The value of a is " << a << " The value of b is " << b ;
+	return 0  ;
+
+    int a , b ;
+    cout << "Hey please enter two numbers" << endl ;
+    cin >> a >> b ;
+    cout << "The value of a is " << a << " The value of b is " << b ;
+
+}
